9fc4c6553f6473c368a553b087f297f5c4ea9c26
WhoMeNope <whomenope@outlook.com>
2018-11-20 19:32:14 +0100

    remove dotenv

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-// read dotenv
-let r = require('dotenv').config()
-
 // setup logger
-const logger = require('./logger.js')
-
-// handle dotenv error
-if (r.error) {
-  logger.error(r.error)
-  logger.info('could not parse dotenv')
-}
-else {
-  logger.info('parsed dotenv succesfully')
-}
+const logger = require('./logger')
 
 // requires
 const app = require('express')()
@@ -25,11 +13,15 @@ require('./email.js')
     app.get('/onboard/:email/:name', function (req, res) {
       if (req.params.email) {
         sender.onboard(req.params.email, req.params.name)
-        res.status(200).send('okay')
+        res
+          .status(200)
+          .send('okay')
       }
       else {
         logger.info('got no email address - ignoring request')
-        res.status(400).send()
+        res
+          .status(400)
+          .send('no email address')
       }
     })
 
@@ -37,11 +29,15 @@ require('./email.js')
     app.get('/unsubscribe/:email', function (req, res) {
       if (req.params.email) {
         sender.unsubscribe(req.params.email)
-        res.status(200).send('okay')
+        res
+          .status(200)
+          .send('okay')
       }
       else {
         logger.info('got no email address - ignoring request')
-        res.status(400).send()
+        res
+          .status(400)
+          .send('no email address')
       }
     })
 
@@ -51,3 +47,4 @@ require('./email.js')
     logger.error(err)
     process.exit(0)
   })
+
